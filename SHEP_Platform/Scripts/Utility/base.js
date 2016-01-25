9dc45054f227a7984cfb5281d61626c5c3bdfa30
Alex <autyan@outlook.com>
2016-01-25 19:26:52 +0800

    添加JS文件

---
+++ SHEP_Platform/Scripts/Utility/base.js
@@ -0,0 +1,27 @@
+﻿var trimStr = function (str) {
+    var re = /^\s+|\s+$/;
+    return !str ? "" : str.replace(re, "");
+}
+
+var IsNullOrEmpty = function (obj) {
+    try {
+        if (obj.length === 0) {
+            return true;
+        }
+
+        return false;
+    }
+    catch (e) {
+        if (!obj) {
+            return true;
+        }
+        if (typeof obj == "string" && trimStr(obj) === "") {
+            return true;
+        }
+
+        if (typeof obj == "number" && isNaN(obj))
+            return true;
+
+        return false;
+    }
+}
