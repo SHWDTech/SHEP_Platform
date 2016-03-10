efe0c06847cb8af73d633f53907ae9b9cdd18842
Alex <autyan@outlook.com>
2016-03-10 17:57:24 +0800

    临时提交，添加非高清摄像头控制模块。

--- SHEP_Platform/Scripts/Page/StatView.js
+++ SHEP_Platform/Scripts/Page/StatView.js
@@ -5,11 +5,14 @@ StatViewer.ViewObject = document.getElementById('StatViewer');
 
 StatViewer.ConnInfo = {};
 
+StatViewer.DeviceInfos = [];
+
 StatViewer.Init = function(info) {
     this.ConnInfo.Server = info.DnsAddr;
     this.ConnInfo.Port = info.Port;
     this.ConnInfo.UserName = info.UserName;
     this.ConnInfo.PassWord = info.PassWord;
+    this.ViewObject.LoginStyle = 1;
     this.Login();
 };
 
@@ -19,8 +22,70 @@ StatViewer.Login = function () {
         return;
     }
 
-    if (!this.ViewObject.LoginServer(this.ConnInfo.Server, this.ConnInfo.Port, this.ConnInfo.UserName, this.ConnInfo.PassWord)) {
+    var logResult = this.ViewObject.LoginServer(this.ConnInfo.Server, this.ConnInfo.Port, this.ConnInfo.UserName, this.ConnInfo.PassWord);
+    if (!logResult) {
         msg.warning("连接视频服务器错误，请稍候，正在尝试重新连接");
-        setTimeout(function() { StatViewer.Login(); }, 30000);
+        setTimeout(function() { this.Login(); }, 30000);
+    } else {
+        this.LoadDeviceInfo();
+
+        setTimeout(function () { StatViewer.StartMonitor(); }, 3000);
     }
+}
+
+StatViewer.LoadDeviceInfo = function() {
+    var devCount = this.ViewObject.StartGetDeviceList();
+
+    for (var i = 0; i < devCount; i++) {
+        var devInfo = this.ViewObject.GetNextDeviceInfo();
+        this.DeviceInfos.push(this.InitDev(devInfo));
+    }
+}
+
+StatViewer.InitDev = function(devInfo) {
+    var dev = {};
+    dev.deviceId = devInfo.split(' ')[0].split(':')[1];
+    return dev;
+}
+
+StatViewer.StartMonitor = function() {
+    this.ViewObject.PlayVideo(this.DeviceInfos[0].deviceId, 0);
+}
+
+
+StatViewer.PTZCtrl = function (cmd) {
+    var ret = this.ViewObject.PTZCtrl(this.DeviceInfos[0].deviceId, 0, cmd);
+    console.log(ret);
+}
+
+StatViewer.Up = function() {
+    this.PTZCtrl(1);
+}
+
+StatViewer.Down = function () {
+    this.PTZCtrl(2);
+}
+
+StatViewer.Left = function () {
+    this.PTZCtrl(3);
+}
+
+StatViewer.Right = function () {
+    this.PTZCtrl(4);
+}
+
+function Up() {
+    StatViewer.Up();
+}
+
+function Down() {
+    StatViewer.Down();
+}
+
+function Left() {
+    StatViewer.Left();
+}
+
+function Right() {
+    StatViewer.Right();
 }
