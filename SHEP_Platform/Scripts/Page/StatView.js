230c5226255dbc29c7dce5a0d86d2feeb3d08343
Alex <autyan@outlook.com>
2016-03-17 18:59:06 +0800

    修改摄像头模块

--- SHEP_Platform/Scripts/Page/StatView.js
+++ SHEP_Platform/Scripts/Page/StatView.js
@@ -7,7 +7,7 @@ StatViewer.ConnInfo = {};
 
 StatViewer.DeviceInfos = [];
 
-StatViewer.Init = function(info) {
+StatViewer.Init = function (info) {
     this.ConnInfo.Server = info.DnsAddr;
     this.ConnInfo.Port = info.Port;
     this.ConnInfo.UserName = info.UserName;
@@ -25,7 +25,7 @@ StatViewer.Login = function () {
     var logResult = this.ViewObject.LoginServer(this.ConnInfo.Server, this.ConnInfo.Port, this.ConnInfo.UserName, this.ConnInfo.PassWord);
     if (!logResult) {
         msg.warning("连接视频服务器错误，请稍候，正在尝试重新连接");
-        setTimeout(function() { this.Login(); }, 30000);
+        setTimeout(function () { this.Login(); }, 30000);
     } else {
         this.LoadDeviceInfo();
 
@@ -33,7 +33,7 @@ StatViewer.Login = function () {
     }
 }
 
-StatViewer.LoadDeviceInfo = function() {
+StatViewer.LoadDeviceInfo = function () {
     var devCount = this.ViewObject.StartGetDeviceList();
 
     for (var i = 0; i < devCount; i++) {
@@ -42,28 +42,33 @@ StatViewer.LoadDeviceInfo = function() {
     }
 }
 
-StatViewer.InitDev = function(devInfo) {
+StatViewer.InitDev = function (devInfo) {
     var dev = {};
     dev.deviceId = devInfo.split(' ')[0].split(':')[1];
     return dev;
 }
 
-StatViewer.StartMonitor = function() {
-    this.ViewObject.PlayVideo(this.DeviceInfos[0].deviceId, 0);
+StatViewer.StartMonitor = function () {
+    this.ViewObject.PlayVideo(this.DeviceInfos[1].deviceId, 0);
 }
 
 
-StatViewer.PTZCtrl = function (cmd) {
-    var ret = this.ViewObject.PTZCtrl(this.DeviceInfos[0].deviceId, 0, cmd);
+StatViewer.PTZCtrl = function (cmd, delay) {
+    var ret = this.ViewObject.PTZCtrl(this.DeviceInfos[1].deviceId, 0, cmd);
     console.log(ret);
+
+    var stop = false;
+    if (delay == null) delay = 200;
+    setTimeout(function () { stop = StatViewer.ViewObject.PTZCtrl(StatViewer.DeviceInfos[1].deviceId, 0, 0) === 0; }, delay);
+    console.log("stoped" + stop);
 }
 
-StatViewer.Up = function() {
-    this.PTZCtrl(1);
+StatViewer.Up = function () {
+    this.PTZCtrl(1, 100);
 }
 
 StatViewer.Down = function () {
-    this.PTZCtrl(2);
+    this.PTZCtrl(2, 100);
 }
 
 StatViewer.Left = function () {
@@ -72,6 +77,31 @@ StatViewer.Left = function () {
 
 StatViewer.Right = function () {
     this.PTZCtrl(4);
+}
+
+StatViewer.Far = function () {
+    this.PTZCtrl(6, 500);
+}
+
+StatViewer.Near = function () {
+    this.PTZCtrl(7, 500);
+}
+
+StatViewer.FocusFar = function () {
+    this.PTZCtrl(8);
+}
+
+StatViewer.FocusNear = function () {
+    this.PTZCtrl(9);
+}
+
+StatViewer.TakePicture = function () {
+    var ret = this.ViewObject.SnapPicture('d:\\201603171.jpg');
+    if (ret === 0) {
+        alert('截图成功');
+    } else {
+        alert('截图失败');
+    }
 }
 
 function Up() {
@@ -88,4 +118,24 @@ function Left() {
 
 function Right() {
     StatViewer.Right();
+}
+
+function Far() {
+    StatViewer.Far();
+}
+
+function Near() {
+    StatViewer.Near();
+}
+
+function FocusFar() {
+    StatViewer.FocusFar();
+}
+
+function FocusNear() {
+    StatViewer.FocusNear();
+}
+
+function TakePicture() {
+    StatViewer.TakePicture();
 }
