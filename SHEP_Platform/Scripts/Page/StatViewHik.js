7e9894ba102dc131a43794e2453e0f0650603456
autyan <autyan@hotmail.com>
2016-04-17 14:56:56 +0800

    添加云台控制JS

---
+++ SHEP_Platform/Scripts/Page/StatViewHik.js
@@ -0,0 +1,77 @@
+﻿//Ajax请求地址
+var ajaxUrl = '/Ajax/Access';
+
+var StatViewer = {};
+
+StatViewer.Control = function(dir) {
+    var param = {
+        'fun': 'cameraMoveControl',
+        'dir': dir
+    }
+
+    $.post(ajaxUrl, param, function (ret) {
+        if (!ret.success) {
+            msg.warning('操作云台发生错误！');
+        }
+    });
+};
+
+StatViewer.Stop = function() {
+    var param = {
+        'fun': 'cameraMoveStop'
+    }
+
+    $.post(ajaxUrl, param);
+}
+
+StatViewer.CapturePicture = function() {
+    var param = {
+        'fun': 'capturePicture'
+    }
+
+    $.post(ajaxUrl, param);
+}
+
+function Up() {
+    StatViewer.Control('Up');
+}
+
+function Down() {
+    StatViewer.Control('Down');
+}
+
+function Left() {
+    StatViewer.Control('Left');
+}
+
+function Right() {
+    StatViewer.Control('Right');
+}
+
+function Far() {
+    StatViewer.Far();
+}
+
+function Near() {
+    StatViewer.Near();
+}
+
+function FocusFar() {
+    StatViewer.FocusFar();
+}
+
+function FocusNear() {
+    StatViewer.FocusNear();
+}
+
+function TakePicture() {
+    StatViewer.TakePicture();
+}
+
+function Stop() {
+    StatViewer.Stop();
+}
+
+function CapturePicture() {
+    StatViewer.CapturePicture();
+}
