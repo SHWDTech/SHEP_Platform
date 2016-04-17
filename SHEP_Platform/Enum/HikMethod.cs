be3d801c98bdc9e8a4290d49fb1a0b2796d2f32c
Alex <autyan@outlook.com>
2016-04-17 14:37:58 +0800

    添加海康支持

---
+++ SHEP_Platform/Enum/HikMethod.cs
@@ -0,0 +1,13 @@
+﻿namespace SHEP_Platform.Enum
+{
+    /// <summary>
+    /// 海康SDK方法
+    /// </summary>
+    public static class HikMethod
+    {
+        /// <summary>
+        /// The get access token
+        /// </summary>
+        public const string GetAccessToken = "token/getAccessToken";
+    }
+}
