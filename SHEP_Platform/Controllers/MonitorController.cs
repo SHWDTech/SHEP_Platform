f78b3be9e8259b9aab8d0f3a72303cbe896a235f
Alex <autyan@outlook.com>
2016-05-13 10:21:50 +0800

    添加容宏摄像头支持

--- SHEP_Platform/Controllers/MonitorController.cs
+++ SHEP_Platform/Controllers/MonitorController.cs
@@ -24,7 +24,8 @@ namespace SHEP_Platform.Controllers
             foreach (var stat in WdContext.StatList)
             {
                 var hour = DateTime.Now.AddHours(-1);
-                var value = DbContext.T_ESHour.FirstOrDefault(item => item.StatId == stat.Id && item.UpdateTime.Hour > hour.Hour);
+                var value =
+                    DbContext.T_ESHour.FirstOrDefault(item => item.StatId == stat.Id && item.UpdateTime.Hour > hour.Hour);
                 var current = DbContext.T_ESMin.Where(obj => obj.StatId == stat.Id).ToList().LastOrDefault();
 
                 var info = new StatHourInfo
@@ -62,7 +63,8 @@ namespace SHEP_Platform.Controllers
             foreach (var stat in WdContext.StatList)
             {
                 var hour = DateTime.Now.AddHours(-1);
-                var value = DbContext.T_ESHour.FirstOrDefault(item => item.StatId == stat.Id && item.UpdateTime.Hour > hour.Hour);
+                var value =
+                    DbContext.T_ESHour.FirstOrDefault(item => item.StatId == stat.Id && item.UpdateTime.Hour > hour.Hour);
                 var current = DbContext.T_ESMin.Where(obj => obj.StatId == stat.Id).ToList().LastOrDefault();
 
                 var info = new StatHourInfo
@@ -89,26 +91,29 @@ namespace SHEP_Platform.Controllers
         public ActionResult ScheduleCompare()
         {
             WdContext.SiteMapMenu.ActionMenu.Name = "按工期进行综合对比";
-            var basic = WdContext.StatList.Where(stat => stat.Stage == (int)Stage.Basic).Select(obj => obj.Id).ToList();
-            var structure = WdContext.StatList.Where(stat => stat.Stage == (int)Stage.Structure).Select(obj => obj.Id).ToList();
+            var basic = WdContext.StatList.Where(stat => stat.Stage == (int) Stage.Basic).Select(obj => obj.Id).ToList();
+            var structure =
+                WdContext.StatList.Where(stat => stat.Stage == (int) Stage.Structure).Select(obj => obj.Id).ToList();
 
             var startDate = DateTime.Now.AddMonths(-1);
-            var basicSource = DbContext.T_ESDay.Where(obj => basic.Contains(obj.StatId) && obj.UpdateTime > startDate).ToList()
+            var basicSource = DbContext.T_ESDay.Where(obj => basic.Contains(obj.StatId) && obj.UpdateTime > startDate)
+                .ToList()
                 .Select(item => new
                 {
-                    TP = double.Parse((item.TP / 1000).ToString("f2")),
+                    TP = double.Parse((item.TP/1000).ToString("f2")),
                     DB = double.Parse(item.DB.ToString("f2")),
                     UpdateTime = item.UpdateTime.ToString("yyyy-MM-dd")
                 });
-            var structureSource = DbContext.T_ESDay.Where(obj => structure.Contains(obj.StatId) && obj.UpdateTime > startDate).ToList()
+            var structureSource = DbContext.T_ESDay.Where(
+                obj => structure.Contains(obj.StatId) && obj.UpdateTime > startDate).ToList()
                 .Select(item => new
                 {
-                    TP = double.Parse((item.TP / 1000).ToString("f2")),
+                    TP = double.Parse((item.TP/1000).ToString("f2")),
                     DB = double.Parse(item.DB.ToString("f2")),
                     UpdateTime = item.UpdateTime.ToString("yyyy-MM-dd")
                 });
 
-            var dict = new Dictionary<string, object> { { "basic", basicSource }, { "structure", structureSource } };
+            var dict = new Dictionary<string, object> {{"basic", basicSource}, {"structure", structureSource}};
 
             var basicList = basicSource.ToList();
             var structureList = structureSource.ToList();
@@ -155,6 +160,19 @@ namespace SHEP_Platform.Controllers
         }
 
 
-        public ActionResult StatViewHik() => View();
+        public ActionResult StatViewHik()
+        {
+            WdContext.SiteMapMenu.ActionMenu.Name = "工程实时状况查看";
+
+            return View();
+        }
+
+
+        public ActionResult StatViewCircle()
+        {
+            WdContext.SiteMapMenu.ActionMenu.Name = "工程实时状况查看";
+
+            return View();
+        }
     }
 }
